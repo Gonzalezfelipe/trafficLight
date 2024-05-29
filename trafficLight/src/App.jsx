8b0a7579ed379fa4e54c96282b4969fa122b00eb
Gonzalezfelipe <felipegr.dev@gmail.com>
2024-05-29 11:39:09 -0400

    Finished

--- trafficLight/src/App.jsx
+++ trafficLight/src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedColor, setSelectedColor] = useState("");
+
+  const changeColor = () => {
+    if (selectedColor === "red") {
+      setSelectedColor("yellow");
+    } else if (selectedColor === "yellow") {
+      setSelectedColor("green");
+    } else {
+      setSelectedColor("red");
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="support"></div>
+      <div className="container-stick d-flex justify-content-center">
+        <div className="stick"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+
+      <div className="trafficlight">
+        <div
+          onClick={() => setSelectedColor("red")}
+          className={"light " + (selectedColor === "red" ? "red glow" : "red")}
+        ></div>
+        <div
+          onClick={() => setSelectedColor("yellow")}
+          className={
+            "light " + (selectedColor === "yellow" ? "yellow glow" : "yellow")
+          }
+        ></div>
+        <div
+          onClick={() => setSelectedColor("green")}
+          className={
+            "light " + (selectedColor === "green" ? "green glow" : "green")
+          }
+        ></div>
+      </div>
+      <div className="container-button d-flex  flex-column justify-content-center mt-5">
+        <button onClick={changeColor} className=" btn btn-success">
+          ¡Cambia de color!
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button
+          onClick={() => setSelectedColor("")}
+          className="btn btn-danger mt-5"
+        >
+          ¡Reset!
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
